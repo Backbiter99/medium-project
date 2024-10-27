8b18255829ca0c3504a3ff4f217738cbcd96967f
Prajwal Jha <prajwaljha@gmail.com>
2024-10-27 13:13:51 +0530

    add root routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import { Signup } from "./pages/Signup.tsx";
 import { Signin } from "./pages/Signin.tsx";
@@ -8,17 +8,16 @@ import { Publish } from "./components/Publish.tsx";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/blog/:id" element={<Blog />} />
-          <Route path="/blogs" element={<Blogs />} />
-          <Route path="/publish" element={<Publish />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/signin" />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/blog/:id" element={<Blog />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/publish" element={<Publish />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
